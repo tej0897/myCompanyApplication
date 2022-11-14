8cb66f4823471f4af88887802003466c04187e46
tej0897 <tej0897@gmail.com>
2022-11-14 22:58:46 +0530

    template - final app code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { CompanyDashboardComponent } from './company-dashboard/company-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    CompanyDashboardComponent
   ],
   imports: [
     BrowserModule,
